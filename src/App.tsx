844648d7001b71ee36ff08d9daa17859c8ab2e1a
Arnaud Laforgue <arnaud.laforgue@gmail.com>
2023-11-17 12:07:03 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import './App.css'
+import Connexion from './Connexion'
+import {
+  RouterProvider,
+  BrowserRouter,
+  createBrowserRouter
+} from "react-router-dom";
+import Home from './Home'
+import Type_de_QCM from './Type_de_QCM';
+
+function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: "/connexion",
+      element: <Connexion />,
+    },
+    {
+      path: "/type_de_qcm",
+      element: <Type_de_QCM />,
+    },
+    {
+      path: "/home",
+      element: <Home />,
+    }
+  ]);
+  return (
+    <>
+      <div>
+        <RouterProvider router={router} />
+      </div>
+    </>
+  )
+}
+
+export default App
